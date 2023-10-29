9a4dbfdf8855247460bff7140f1e6ddf5a5d1b29
Myryk <myryk@posteo.de>
2023-10-29 23:43:14 +0100

    outdated version

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "./get_next_line.h"
 #include <unistd.h>
 
 void	ft_bzero(void *s, size_t n)
